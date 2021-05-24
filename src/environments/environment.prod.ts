9d68bbef30bfdf708e190a3e91e8167755e6231a
Iruobe Akhigbe <iruobeakhigbe@gmail.com>
2021-05-24 10:27:05 +0100

    Production Environment.ts file updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   TOKEN: 'fiap_upperlink_token_3149567',
   ROLE: 'fiap_upperlink_role_97323456',
   TOKEN_DATE: 'fiap_upperlink_token_expiry_date',
-  DATE_NOW: 'fiap_upperlink_token_date',
+  DATE_NOW: 'fiap_upperlink_token_date_now',
   PRIVATE_KEY: `3FIAP-UPPERLINKgfdertyuhghgfrtyhbdrtyuytfhtrtyuhgghtrdfgytdghtrtre2345678uyfdvcxsaqq5wertyuijbvcxdffdcvgfghyt6t`,
   USERTOKEN: 'fiap_upperlink_user_56789',
   USERID: 'fiap_upperlink_user_id',
@@ -16,6 +16,7 @@ export const environment = {
   AUDITCOUNT: 'audit_count_number',
   BANKCOUNT: 'bank_count_number',
   TOKENEXPIRYCOUNT: 'token_expiry_count',
+  LOGOUTTIME: 'log_out_time',
   ACTION_KEY: '12384-FIAP-UPPERLINK9-2tred;sdfguytr08j-987to890pojrt9-2tred;sdfguytr08j-iruobeakhigbe',
   URL_VALIDATION:  /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi,
   EMAIL_VALIDATION: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
